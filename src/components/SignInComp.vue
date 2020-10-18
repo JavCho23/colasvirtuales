7eb89b32f740debc10c6301fe15084adb484638e
Ale <jalexa_510@hotmail.com>
2020-10-17 19:13:45 -0500

    Interfaz Login

--- src/components/SignInComp.vue
+++ src/components/SignInComp.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    hola
-  </div>
+  <v-card class="pa-4">
+    <v-card-title class="d-flex justify-center">
+      Iniciar sesion
+    </v-card-title>
+    <v-text-field 
+            label="Usuario"
+            placeholder="tugatita123"
+    ></v-text-field>
+     <v-text-field 
+            label="Contraseña"
+            placeholder="********"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show ? 'text' : 'password'"
+             @click:append="show = !show"
+    ></v-text-field>
+    <v-card-actions class="d-flex justify-center">
+      <v-btn color="#ffac0e">
+        Iniciar
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        show: false,
+      }
+    },
+  }
+</script>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <signInComp />
-  </div>
+  <v-container>
+  <v-row class="d-flex justify-center">
+    <v-col cols="12" md="4">
+        <signInComp />
+    </v-col>
+  </v-row>
+  </v-container>
 </template>
 <script>
 import signInComp from "@/components/SignInComp.vue";
